04bd2d3e88fc21773e7265b87f9ccf2d59cf0bbf
nicolas <arias_npj_26@hotmail.com>
2019-05-18 16:27:56 -0300

    Agrego Punto 1

---
+++ Punto 1/Punto 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Punto_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un numero\n");
+            int num = int.Parse(Console.ReadLine());
+            
+            if (num <= 0)
+            {
+                Console.Write(num);
+            }
+            else 
+            {
+                while (num > 10) 
+                {
+                    Console.Write(num % 10);
+                    num = num / 10;
+                }
+            }
+            Console.Write(num);
+            Console.ReadKey();
+        }
+    }
+}
